2e162e679397d917c4aef35148c23f85e32056f9
fariasgustavo <santosgustavo60@gmail.com>
2019-04-03 16:45:38 -0300

    Setup migrations

---
+++ database/migrations/2019_04_03_194322_task.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Task extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('completed');
+            $table->string('description');
+            $table->integer('project_id');
+            $table->timestamps();
+        });
+
+        Schema::table('projects', function (Blueprint $table) {
+            $table->integer('project_id');
+        
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-    changeStatusTask();
-
-    function changeStatusTask(){
-        $("#edit-project .tasks td span").on("click",function(){
-            console.log(1);
-        });
-    }
